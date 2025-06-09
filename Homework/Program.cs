3b4e74630c76e0e065ad659b9e00b3dff257808a
NKugelman <kugelmannaftoli@gmail.com>
2025-06-09 18:38:59 -0400

    hw 49

---
+++ Homework/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Clothing_Store {
+   
+        
+    public class Program {
+        static string[] colors = { "red", "green", "orange" };
+        static string[] design = { "stripe", "solid", "suede" };
+        public class Shirt {
+           public string Color { get; set; }
+           public string Design { get; set; }
+            public Shirt(string color, string design) {
+                this.Color = color;
+                this.Design = design;
+                }
+
+            public override string ToString() {
+                return $"{Color}:{Design}";
+                }
+            }
+
+            static void Main(string[] args) {
+            List<Shirt> shirts = new List<Shirt>();
+            for (int i = 0; i < colors.Length; i++) {
+                for (int j = 0; j < design.Length; j++) {
+                    shirts.Add(new Shirt(colors[i], design[j]));
+
+                    }
+
+                }
+            foreach(var shirt in shirts) {
+                Console.WriteLine(shirt);
+                }
+
+            }
+        }
+    }
