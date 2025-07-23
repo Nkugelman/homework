54fbfa2f2e0bcc5e4f11301641571a80ffc28083
NKugelman <kugelmannaftoli@gmail.com>
2025-07-23 14:16:17 -0400

    fixed folders

---
+++ Html/HW/shares/practice.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            System.Console.WriteLine(1 + 1);
+        }
+    }
+}
