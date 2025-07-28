836997268bde9870a4a0d033845913cc64e53570
NKugelman <kugelmannaftoli@gmail.com>
2025-07-28 14:55:14 -0400

    test comments

---
+++ Homework/js/61/one.js
@@ -0,0 +1,11 @@
+console.log(x);
+
+let x = 6;
+
+const yourname = prompt("what is your name");
+alert('your name is' + yourname);
+const templateString = ` hello ${yourname}
+i am 
+a multiliner`
+alert(templateString)
+
