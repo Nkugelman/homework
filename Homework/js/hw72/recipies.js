adcad727506594782c700261d187a6409b46c733
Naftoli Kugelman <kugelmannaftoli@gmail.com>
2025-10-25 22:23:57 -0400

    hw 72

---
+++ Homework/js/hw72/recipies.js
@@ -0,0 +1,61 @@
+(async function() {
+  'use strict';
+
+  const recipeList = $('#recipes');
+  const recipeNameElement = $('#name');
+  const recipeImageElement = $('#picture');
+  const recipeIngredientsElement = $('#ingredients');
+  const recipeElements = $('.has-recipe');
+  const noRecipeElements = $('.no-recipe');
+  const errorElements = $('.error');
+
+  async function fetchJson(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+      return await response.json();
+    } catch (error) {
+      errorElements.text(`Fetch error: ${error.message}`);
+    }
+  }
+
+  try {
+   
+    const recipes = await fetchJson('recipes.json');
+
+    recipes.forEach(recipe => {
+      recipeList.append(`<option value="${recipe.file}">${recipe.name}</option>`);
+    });
+
+   
+    recipeList.change(async e => {
+      try {
+        const r = await fetch(`${e.target.value}`);
+        if (!r.ok) {
+          throw new Error(`${r.status} - ${r.statusText}`);
+        }
+        const recipe = await r.json();
+
+        // Update UI
+        recipeElements.show();
+        noRecipeElements.hide();
+
+        recipeNameElement.text(recipe.name);
+        recipeImageElement.attr('src', recipe.picture);
+        recipeIngredientsElement.empty();
+
+        recipe.ingredients.forEach(ingredient => {
+          recipeIngredientsElement.append(`<li>${ingredient}</li>`);
+        });
+
+      } catch (error) {
+        errorElements.text(`Fetch error: ${error.message}`);
+      }
+    });
+  } catch (error) {
+    errorElements.text(`Fetch error: ${error.message}`);
+  }
+
+})();
