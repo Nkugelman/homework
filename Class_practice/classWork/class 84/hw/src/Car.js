08054aa4e7c6ed0fe9dc43227d212f9a502ad1ff
Naftoli Kugelman <kugelmannaftoli@gmail.com>
2025-12-02 20:12:37 -0500

    Remove hw from repo but keep locally

--- Class_practice/classWork/class 84/hw/src/Car.js
+++
@@ -1,9 +0,0 @@
-export function Car(color){
-    this.color =color;
-};
-
-
-Car.prototype.drive =function (){
-    console.log(`${this.color} car is driving`);
-    
-}
--- Class_practice/classWork/class 84/hw/src/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import {Car} from './Car.Js';
-const car =new Car('red');
-console.log(car);
-car.drive();
-
-
-
-
