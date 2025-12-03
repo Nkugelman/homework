d50ad1c5a0461885e543dbfff61675eeca40e249
Naftoli Kugelman <kugelmannaftoli@gmail.com>
2025-12-02 19:57:56 -0500

    hw 84

---
+++ Homework/js/hw84/hw84/src/Address.jsx
@@ -0,0 +1,10 @@
+export default function Address(props){
+const {street ,city,state,zip} =props;
+
+return(
+    <div>
+        <h1>Address</h1>
+        <p>{street} {city} {state} {zip}</p>
+    </div>
+)
+}
---
+++ Homework/js/hw84/hw84/src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+
+import './App.css'
+import Address from './Address'
+
+function App() {
+ 
+
+  return (
+    <>
+   <Address street ="600 7th ave" city ="lakewood" state ="NJ" zip="08701"/>
+    </>
+  )
+}
+
+export default App
