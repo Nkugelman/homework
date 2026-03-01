b8952043013c79c394e521df35816af4c7babcab
Naftoli Kugelman <kugelmannaftoli@gmail.com>
2026-03-01 17:18:13 -0500

    hw104

---
+++ Homework/Node/hw104/express-sessions/app.js
@@ -0,0 +1,68 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'secret123',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+app.get('/',(req,res)=>{
+  if(!req.session.visits){
+    req.session.visits-0
+  };
+  req.session.visits++;
+  res.render('index',{
+    name: req.session.name || "Guest",
+    visits:req.session.visits
+  })
+})
+app.get('/name', (req, res) => {
+  res.render('name');
+});
+app.post('/name', (req, res) => {
+
+  req.session.name = req.body.name;
+
+  res.redirect('/');
+});
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
